3286b7b26f28c970ccadd8621fd680408ad2cf4c
Nirob Sarker <nirobsarker012@gmail.com>
2025-09-11 17:19:04 +0600

    add: top-dishesh-food

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,26 +1,48 @@
-import React, { use } from "react";
+import React, { useContext, useState } from "react";
 import { StoreContext } from "../context/StoreContext";
 import FoodItem from "../FoodItem/FoodItem";
 
 const FoodDisplay = ({ category }) => {
-  const { food_list } = use(StoreContext);
+  const { food_list } = useContext(StoreContext);
+  const [showAll, setShowAll] = useState(false);
+
+  // Filter by category
+  const filteredFoods =
+    category === "All"
+      ? food_list
+      : food_list.filter((item) => item.category === category);
+
+  // Show only 6 items if not showing all
+  const displayedFoods = showAll ? filteredFoods : filteredFoods.slice(0, 8);
+
   return (
     <div className="mt-[30px]" id="food-display">
       <h2 className="text-[max(2vw,24px)] font-bold">Top dishes near you</h2>
+
       <div className="grid grid-cols-[repeat(auto-fill,minmax(240px,1fr))] mt-[30px] gap-[30px]">
-        {food_list.map((item, index) => {
-          return (
-            <FoodItem
-              key={index}
-              id={item._id}
-              name={item.name}
-              description={item.description}
-              image={item.image}
-              price={item.price}
-            />
-          );
-        })}
+        {displayedFoods.map((item, index) => (
+          <FoodItem
+            key={index}
+            id={item._id}
+            name={item.name}
+            description={item.description}
+            image={item.image}
+            price={item.price}
+          />
+        ))}
       </div>
+
+      {/* Show All / Show Less Button */}
+      {filteredFoods.length > 8 && (
+        <div className="flex justify-center mt-6">
+          <button
+            onClick={() => setShowAll(!showAll)}
+            className="px-6 py-2 bg-text-primary border border-text-primary text-white rounded-lg shadow-md hover:bg-transparent hover:text-text-primary transition cursor-pointer"
+          >
+            {showAll ? "Show Less" : "Show All"}
+          </button>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { use } from "react";
 import { assets } from "../../assets/frontend_assets/assets";
+import { StoreContext } from "../context/StoreContext";
 
 const FoodItem = ({ id, name, price, description, image }) => {
+  const { cartItems, addToCart, removeFromCart } = use(StoreContext);
   return (
     <div className="w-full mx-auto rounded-[15px] shadow-lg">
-      <div className="overflow-hidden rotate-[15px]">
+      <div className="overflow-hidden rotate-[15px] relative">
         <img
           src={image}
           alt=""
           className="w-full rounded-[15px] transform transition-transform duration-300 hover:scale-107"
         />
+        {!cartItems[id] ? (
+          <img
+            className="w-[35px] absolute bottom-[15px] right-[15px] rounded-[50%] cursor-pointer"
+            onClick={() => addToCart(id)}
+            src={assets.add_icon_white}
+            alt=""
+          />
+        ) : (
+          <div className="absolute bottom-[15px] right-[15px] flex items-center gap-[10px] rounded-[50%] bg-white">
+            <img
+              onClick={() => removeFromCart(id)}
+              src={assets.remove_icon_red}
+              alt=""
+              className="w-[30px] cursor-pointer"
+            />
+            <p>{cartItems[id]}</p>
+            <img
+              className="w-[30px] cursor-pointer"
+              onClick={() => addToCart(id)}
+              src={assets.add_icon_green}
+              alt=""
+            />
+          </div>
+        )}
       </div>
       <div className="p-4 ">
         <div className="flex justify-between items-center mb-[10px]">
