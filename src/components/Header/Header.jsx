6f143c390108ebaa1ead37de587e4e61d79acf4e
Nirob Sarker <nirobsarker012@gmail.com>
2025-09-11 13:34:12 +0600

    add: hero-section

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="h-[34vw] m-[30px] mx-auto bg-[url('/header_img.png')] bg-no-repeat bg-contain relative">
+      <div className="absolute flex flex-col items-start gap-[1.5vw] max-w-[50%] bottom-[10%] left-[6vw]">
+        <h2 className="font-bold text-white text-[max(4.5vw,22px)]">
+          Order your favourite for here
+        </h2>
+        <p className="text-white text-[1vw]">
+          Choose form a diverse menu featuring a delectable array of dishes
+          crafted with the fantastisfy your cravings and elevate your dining
+          experience, on deleicious meal at a time.
+        </p>
+        <button className="border text-[#747474] font-bold py-[1vw] px-[2.3vw] bg-white text-[max(1vw,13px)] rounded-[50px] cursor-pointer">
+          View Menu
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,25 +4,45 @@ import { assets } from "../../assets/frontend_assets/assets";
 const Navbar = () => {
   const [menu, setMenu] = useState("Home");
   return (
-    <div className="py-[20px] flex justify-between items-center">
+    <nav className="py-[20px] flex justify-between items-center">
       <img src={assets.logo} alt="logo" className="w-[150px]" />
       <ul className="flex gap-[20px] text-items text-[18px]">
-        <li className={menu === "Home" ? "active:" : ""}>Home</li>
-        <li className={menu === "Menu" ? "active:" : ""}>Menu</li>
-        <li className={menu === "Mobile-app" ? "active:" : ""}>Mobile-app</li>
-        <li className={menu === "Contact US" ? "active:" : ""}>Contact US</li>
+        <li
+          onClick={() => setMenu("Home")}
+          className={menu === "Home" ? "active" : ""}
+        >
+          Home
+        </li>
+        <li
+          onClick={() => setMenu("Menu")}
+          className={menu === "Menu" ? "active" : ""}
+        >
+          Menu
+        </li>
+        <li
+          onClick={() => setMenu("Mobile-app")}
+          className={menu === "Mobile-app" ? "active" : ""}
+        >
+          Mobile-app
+        </li>
+        <li
+          onClick={() => setMenu("Contact US")}
+          className={menu === "Contact US" ? "active" : ""}
+        >
+          Contact US
+        </li>
       </ul>
       <div className="flex items-center gap-[40px]">
         <img src={assets.search_icon} alt="" />
-        <div className="">
+        <div className="relative">
           <img src={assets.basket_icon} alt="" />
-          <div className=""></div>
+          <div className="absolute min-w-[10px] min-h-[10px] bg-[#e84118] -top-4 -right-4 rounded-[5px]"></div>
         </div>
         <button className="bg-transparent text-[16px] text-items border border-t-amber-700 py-[10px] px-[30px] cursor-pointer rounded-[50px] hover:bg-[#fff4f2] transition-all duration-300">
           Sign In
         </button>
       </div>
-    </div>
+    </nav>
   );
 };
 
